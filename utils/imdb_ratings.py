d3c4d83431dedf7de6889a829d09848da6ff2dab
ris-iris <sagitova.ma@phystech.edu>
2023-12-01 08:58:26 +0100

    modify imdb ratings

--- utils/imdb_ratings.py
+++ utils/imdb_ratings.py
@@ -1,19 +1,29 @@
 import pandas as pd
 
-def get_imdb_ratings(chunksize=None):
+def get_imdb_ratings(imdb_titles, movies):
     imdb_rating = pd.read_csv('data/IMDB/ratings.tsv', sep='\t')
-    if chunksize is None:
-        imdb_titles = pd.read_csv('data/IMDB/titles.tsv', sep='\t', low_memory=False)
-    else:
-        imdb_titles = next(pd.read_csv('data/IMDB/titles.tsv', sep='\t', low_memory=False, chunksize=chunksize))
 
     imdb_titles = imdb_titles[(imdb_titles['titleType'] == 'movie')].reset_index(drop=True)
     imdb_titles = imdb_titles[imdb_titles['startYear'].apply(lambda x: x!= '\\N' and int(x) < 2013)]
 
-    title_and_rating = imdb_titles.merge(imdb_rating, left_on='tconst', right_on='tconst')
+    title_and_rating = imdb_titles.merge(imdb_rating, left_on='tconst', right_on='tconst')[['primaryTitle', 'startYear', 'averageRating', 'numVotes']]
+    title_and_rating = title_and_rating.merge(movies, left_on=['primaryTitle', 'startYear'], right_on=['title', 'release_year'])
 
-    title_and_rating['sumRating'] = title_and_rating['averageRating']*title_and_rating['numVotes']
-    title_and_rating = title_and_rating.groupby('primaryTitle').agg({'sumRating':'sum', 'numVotes':'sum'}).reset_index()
-    title_and_rating['averageRating'] = title_and_rating['sumRating']/title_and_rating['numVotes']
+    return title_and_rating
 
-    return title_and_rating[['primaryTitle', 'averageRating']]
+def movies_with_imdb_rating(movies):
+    def release_year(x):
+        if type(x) is str:
+            return x.split('-')[0]
+        else:
+            return x
+
+    movies['release_year'] = movies['release_date'].apply(release_year)
+    
+    chunksize = 1000000
+
+    list_of_dfs = []
+    for df in pd.read_csv('data/IMDB/titles.tsv', sep='\t', low_memory=False, chunksize=chunksize):
+        title_and_rating = get_imdb_ratings(df, movies)
+        list_of_dfs.append(title_and_rating)
+    return pd.concat(list_of_dfs)
